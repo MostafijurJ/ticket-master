ff41b176e27b1f50a6d922cd91ad5b45165f7211
Mostafijur <mostafijur.rahman@brainstation-23.com>
2025-01-22 13:10:41 +0600

    WIP: minor changes

--- gateway-service/src/main/java/com/learn/ms/gateway/config/RateLimiterConfig.java
+++ gateway-service/src/main/java/com/learn/ms/gateway/config/RateLimiterConfig.java
@@ -3,17 +3,17 @@ package com.learn.ms.gateway.config;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ObjectUtils;
 
 @Configuration
 public class RateLimiterConfig {
-
     @Bean
     public KeyResolver remoteAddrKeyResolver() {
         return exchange -> {
             String clientIp = exchange.getRequest()
                     .getHeaders()
                     .getFirst("X-Forwarded-For");
-            if (clientIp == null) {
+            if (ObjectUtils.isEmpty(clientIp)) {
                 clientIp = exchange.getRequest()
                         .getRemoteAddress()
                         .getAddress()
